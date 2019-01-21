5b2e6d62449a9779473ae8e80b83b2cefefa0f29
Luca Realdi <luca.realdi@opencontent.it>
2019-01-21 17:00:25 +0100

    fix youtube endpoint

--- classes/oembedprovider_youtube.php
+++ classes/oembedprovider_youtube.php
@@ -5,9 +5,9 @@ class oembedprovider_youtube implements oEmbedProviderInterface
     static public function definition()
     {
         return array(
-            '#https?://(www\.)?youtube.com/watch.*#i'         => array( 'http://www.youtube.com/oembed',            true  ),
-            '#https?://(www\.)?youtube.com/playlist.*#i'      => array( 'http://www.youtube.com/oembed',            true  ),
-            'https?://youtu.be/*'                             => array( 'http://www.youtube.com/oembed',            false )
+            '#https?://(www\.)?youtube.com/watch.*#i'         => array( 'https://www.youtube.com/oembed',            true  ),
+            '#https?://(www\.)?youtube.com/playlist.*#i'      => array( 'https://www.youtube.com/oembed',            true  ),
+            'https?://youtu.be/*'                             => array( 'https://www.youtube.com/oembed',            false )
         );
     }
 }
